c903a0fc2e771c7ac288acce686a5e745e5962db
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2021-02-05 21:31:56 +0530

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -8,7 +8,7 @@ dynamodb_table_info = boto3.resource('dynamodb', region_name='us-east-1')
 dynamodb_table = dynamodb_table_info.Table('Res2Counter')
 
 def test_of_func():
-    response = requests.get("https://4u2lic1984.execute-api.us-east-1.amazonaws.com/Prod/vCount")
+    response = requests.get("https://vq1m5h3mbj.execute-api.us-east-1.amazonaws.com/Prod/vCount")
     assert response.status_code == 200
     assert response.headers['content-type'] == 'application/json'
 
